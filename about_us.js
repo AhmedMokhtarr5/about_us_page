3f0c0189be65cc60bb5bb62ab0471308617a9b9c
AhmedMokhtarr5 <111442207+AhmedMokhtarr5@users.noreply.github.com>
2022-08-17 17:17:57 +0200

    Delete about_us.js

--- about_us.js
+++
@@ -1,9 +0,0 @@
-function about_us_submit(){
-    if ( confirm("Are you sure you want to send this message")==true)
-    {
-        alert("Message has been sent");
-
-    }else if( confirm("Are you sure you want to send this message")==false){
-        alert("Message has been canceled");
-    }    
-}
